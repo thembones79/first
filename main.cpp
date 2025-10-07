8ed6b7533fabdde784305639324f1eb60ad18799
thembones79 <michal.szumnarski@gmail.com>
2025-10-07 19:35:58 +0200

    moving circle

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include "raylib.h"
 int main() {
-  InitWindow(320, 200, "Siema");
+  int width{320};
+  int height{200};
+
+  InitWindow(width, height, "Siema");
+
+  int circle_x{160};
+  int circle_y{100};
+
+  SetTargetFPS(60);
+
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RED);
-    DrawCircle(160, 100, 15, BLUE);
+
+    // Game logic begins
+
+    DrawCircle(circle_x, circle_y, 15, BLUE);
+
+    if (IsKeyDown(KEY_D)) {
+      circle_x = circle_x + 10;
+    }
+
+    if (IsKeyDown(KEY_W)) {
+      circle_y = circle_y - 10;
+    }
+
+    if (IsKeyDown(KEY_A)) {
+      circle_x = circle_x - 10;
+    }
+
+    if (IsKeyDown(KEY_S)) {
+      circle_y = circle_y + 10;
+    }
+
+    // Game logic ends
+
     EndDrawing();
   }
 }
