02ce0c24d631f6f3648f4b6f369d417745cc9edc
thembones79 <michal.szumnarski@gmail.com>
2025-10-08 17:27:01 +0200

    rectangle

--- main.cpp
+++ main.cpp
@@ -1,36 +1,37 @@
 #include "raylib.h"
 int main() {
-  int width{320};
+  int width{350};
   int height{200};
 
   InitWindow(width, height, "Siema");
 
-  int circle_x{160};
+  int circle_x{175};
   int circle_y{100};
 
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
-    ClearBackground(RED);
+    ClearBackground(WHITE);
 
     // Game logic begins
 
-    DrawCircle(circle_x, circle_y, 15, BLUE);
+    DrawCircle(circle_x, circle_y, 25, BLUE);
+    DrawRectangle(300, 0, 50, 50, RED);
 
-    if (IsKeyDown(KEY_D)) {
+    if (IsKeyDown(KEY_D) && circle_x < width) {
       circle_x = circle_x + 10;
     }
 
-    if (IsKeyDown(KEY_W)) {
+    if (IsKeyDown(KEY_W) && circle_y > 0) {
       circle_y = circle_y - 10;
     }
 
-    if (IsKeyDown(KEY_A)) {
+    if (IsKeyDown(KEY_A) && circle_x > 0) {
       circle_x = circle_x - 10;
     }
 
-    if (IsKeyDown(KEY_S)) {
+    if (IsKeyDown(KEY_S) && circle_y < height) {
       circle_y = circle_y + 10;
     }
 
