db064a31b4d93f2d17cd299d1d924223d0ba5b07
thembones79 <michal.szumnarski@gmail.com>
2025-10-10 17:32:23 +0200

    its movin

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "raylib.h"
 int main() {
-  int width{350};
-  int height{200};
+  int width{800};
+  int height{450};
 
   InitWindow(width, height, "Siema");
 
-  int circle_x{175};
-  int circle_y{100};
+  int circle_x{200};
+  int circle_y{200};
+
+  int axe_x{400};
+  int axe_y{0};
 
   SetTargetFPS(60);
 
@@ -17,7 +20,9 @@ int main() {
     // Game logic begins
 
     DrawCircle(circle_x, circle_y, 25, BLUE);
-    DrawRectangle(300, 0, 50, 50, RED);
+    DrawRectangle(axe_x, axe_y, 50, 50, RED);
+
+    axe_y = axe_y + 10;
 
     if (IsKeyDown(KEY_D) && circle_x < width) {
       circle_x = circle_x + 10;
