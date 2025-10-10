5b258a45d67ca6950e182c5206dc82c2da4878f6
thembones79 <michal.szumnarski@gmail.com>
2025-10-10 18:49:49 +0200

    its animatin

--- main.cpp
+++ main.cpp
@@ -7,13 +7,23 @@ int main() {
 
   int circle_x{200};
   int circle_y{200};
+  int circle_radius{25};
+  int l_circle_x{circle_x - circle_radius};
+  int r_circle_x{circle_x + circle_radius};
+  int u_circle_y{circle_y - circle_radius};
+  int b_circle_y{circle_y + circle_radius};
 
   int axe_x{400};
   int axe_y{0};
+  int axe_length{50};
+  int l_axe_x{axe_x};
+  int r_axe_x{axe_x + axe_length};
+  int u_axe_y{axe_y};
+  int b_axe_y{axe_y + axe_length};
 
   int direction{10};
 
-  SetTargetFPS(60);
+  SetTargetFPS(240);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
@@ -21,29 +31,29 @@ int main() {
 
     // Game logic begins
 
-    DrawCircle(circle_x, circle_y, 25, BLUE);
-    DrawRectangle(axe_x, axe_y, 50, 50, RED);
+    DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+    DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
     axe_y += direction;
 
-    if (axe_y > height) {
+    if (axe_y > height || axe_y < 0) {
       direction = -direction;
     }
 
     if (IsKeyDown(KEY_D) && circle_x < width) {
-      circle_x += direction;
+      circle_x += 10;
     }
 
     if (IsKeyDown(KEY_W) && circle_y > 0) {
-      circle_y -= direction;
+      circle_y -= 10;
     }
 
     if (IsKeyDown(KEY_A) && circle_x > 0) {
-      circle_x -= direction;
+      circle_x -= 10;
     }
 
     if (IsKeyDown(KEY_S) && circle_y < height) {
-      circle_y += direction;
+      circle_y += 10;
     }
 
     // Game logic ends
