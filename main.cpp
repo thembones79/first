cebd4d39101ce3ac2085d813c280a0b257599b09
thembones79 <michal.szumnarski@gmail.com>
2025-10-11 19:56:37 +0200

    the end

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main() {
 
   InitWindow(width, height, "Siema");
 
+  bool collision_with_axe{false};
+
   int circle_x{200};
   int circle_y{200};
   int circle_radius{25};
@@ -31,29 +33,48 @@ int main() {
 
     // Game logic begins
 
-    DrawCircle(circle_x, circle_y, circle_radius, BLUE);
-    DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
+    bool collision_with_axe =
+        (b_axe_y >= u_circle_y) && (u_axe_y <= b_circle_y) &&
+        (l_axe_x <= r_circle_x) && (r_axe_x >= l_circle_x);
 
-    axe_y += direction;
+    if (collision_with_axe) {
+      DrawText("Game Over!", 300, 200, 40, RED);
+    } else {
 
-    if (axe_y > height || axe_y < 0) {
-      direction = -direction;
-    }
+      l_circle_x = circle_x - circle_radius;
+      r_circle_x = circle_x + circle_radius;
+      u_circle_y = circle_y - circle_radius;
+      b_circle_y = circle_y + circle_radius;
 
-    if (IsKeyDown(KEY_D) && circle_x < width) {
-      circle_x += 10;
-    }
+      l_axe_x = axe_x;
+      r_axe_x = axe_x + axe_length;
+      u_axe_y = axe_y;
+      b_axe_y = axe_y + axe_length;
 
-    if (IsKeyDown(KEY_W) && circle_y > 0) {
-      circle_y -= 10;
-    }
+      DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+      DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
-    if (IsKeyDown(KEY_A) && circle_x > 0) {
-      circle_x -= 10;
-    }
+      axe_y += direction;
 
-    if (IsKeyDown(KEY_S) && circle_y < height) {
-      circle_y += 10;
+      if (axe_y > height || axe_y < 0) {
+        direction = -direction;
+      }
+
+      if (IsKeyDown(KEY_D) && circle_x < width) {
+        circle_x += 10;
+      }
+
+      if (IsKeyDown(KEY_W) && circle_y > 0) {
+        circle_y -= 10;
+      }
+
+      if (IsKeyDown(KEY_A) && circle_x > 0) {
+        circle_x -= 10;
+      }
+
+      if (IsKeyDown(KEY_S) && circle_y < height) {
+        circle_y += 10;
+      }
     }
 
     // Game logic ends
